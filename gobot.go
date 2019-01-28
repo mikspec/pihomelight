ccc0617941330b92867f6432b0982eb4226f7e39
Mikspec <mikspec@mikspec.pl>
2019-01-28 16:57:38 +0100

    Initial version

---
+++ gobot.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/raspi"
+)
+
+// GetRobot returns configured raspi robot - PIR sensor + relay
+func GetRobot(pirPin, relayPin string, delay int) *gobot.Robot {
+	r := raspi.NewAdaptor()
+	sensor := gpio.NewPIRMotionDriver(r, pirPin)
+	relay := gpio.NewRelayDriver(r, relayPin)
+
+	work := func() {
+		sensor.On(gpio.MotionDetected, func(data interface{}) {
+			fmt.Println(gpio.MotionDetected)
+			// Light on
+			relay.On()
+			timer := time.NewTimer(time.Duration(delay) * time.Second)
+			go func() {
+				<-timer.C
+				// Light off
+				relay.Off()
+			}()
+		})
+	}
+
+	robot := gobot.NewRobot("pilight",
+		[]gobot.Connection{r},
+		[]gobot.Device{sensor, relay},
+		work,
+	)
+
+	return robot
+}
