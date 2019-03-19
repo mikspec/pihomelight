46ae3156fcf0a26055a65bf508c2be188cca1503
Mikspec <mikspec@mikspec.pl>
2019-03-19 16:01:01 +0100

    Introduced light on counter to prevent switch off light for multiple motion detections

--- gobot.go
+++ gobot.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	"gobot.io/x/gobot"
@@ -14,18 +15,33 @@ func GetRobot(pirPin, relayPin string, delay int) *gobot.Robot {
 	r := raspi.NewAdaptor()
 	sensor := gpio.NewPIRMotionDriver(r, pirPin)
 	relay := gpio.NewRelayDriver(r, relayPin)
+	mutex := sync.Mutex{}
+	cnt := 0
 
 	work := func() {
 
 		sensor.On(gpio.MotionDetected, func(data interface{}) {
 			fmt.Println(gpio.MotionDetected)
-			// Light on
+
+			// Protect counter
+			mutex.Lock()
+			// Light on - low state switch on the relay
 			relay.Off()
+			cnt++
 			timer := time.NewTimer(time.Duration(delay) * time.Second)
+			mutex.Unlock()
+
 			go func() {
 				<-timer.C
-				// Light off
-				relay.On()
+				// Protect counter
+				mutex.Lock()
+				cnt--
+				// Light off only for last call
+				if cnt == 0 {
+					// Light off - high state switch off the relay
+					relay.On()
+				}
+				mutex.Unlock()
 			}()
 		})
 	}
